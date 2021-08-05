1e49eaa9576b697ceda36bd1d5e6824925b2718c
LOKESH <lokeshdeepu12@gmail.com>
2021-08-04 21:34:39 -0400

    new-commit

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
-terraform {
-  backend "gcs" {
-    bucket = "terraform-state-file-test-0"
-    prefix = "terraform-state-file/"
-  }
-}
+# terraform {
+#   backend "gcs" {
+#     bucket = "terraform-state-file-test-0"
+#     prefix = "terraform-state-file/"
+#   }
+# }
